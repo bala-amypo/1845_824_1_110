5fd1ad7fb4a9c9714e16ecff61f5d8851ffce202
bala-amypo <bala@amypo.in>
2025-12-17 09:21:27 +0000

    User commit

--- src/main/java/com/example/demo/entity/ResourceRequest.java
+++ src/main/java/com/example/demo/entity/ResourceRequest.java
@@ -33,15 +33,34 @@ public class ResourceRequest {
         return resourceType;
          }
     public void setResourceType(String resourceType) { 
-        this.resourceType = resourceType; }
-    public User getRequestedBy() { return requestedBy; }
+        this.resourceType = resourceType;
+         }
+    public User getRequestedBy() {
+         return requestedBy; 
+         }
     public void setRequestedBy(User requestedBy) { this.requestedBy = requestedBy; }
-    public LocalDateTime getStartTime() { return startTime; }
-    public void setStartTime(LocalDateTime startTime) { this.startTime = startTime; }
-    public LocalDateTime getEndTime() { return endTime; }
-    public void setEndTime(LocalDateTime endTime) { this.endTime = endTime; }
-    public String getPurpose() { return purpose; }
-    public void setPurpose(String purpose) { this.purpose = purpose; }
-    public String getStatus() { return status; }
-    public void setStatus(String status) { this.status = status; }
+    public LocalDateTime getStartTime() {
+         return startTime; 
+         }
+    public void setStartTime(LocalDateTime startTime) {
+         this.startTime = startTime;
+          }
+    public LocalDateTime getEndTime() {
+         return endTime; 
+         }
+    public void setEndTime(LocalDateTime endTime) { 
+        this.endTime = endTime;
+         }
+    public String getPurpose() { 
+        return purpose;
+         }
+    public void setPurpose(String purpose) { 
+        this.purpose = purpose; 
+        }
+    public String getStatus() {
+         return status;
+          }
+    public void setStatus(String status) { 
+        this.status = status;
+         }
 }
