d8925ca3d6bb1e885d8506b497c2c801078c59f9
bala-amypo <bala@amypo.in>
2025-12-26 16:07:23 +0000

    User commit

--- src/main/java/com/example/demo/repository/AllocationRuleRepository.java
+++ src/main/java/com/example/demo/repository/AllocationRuleRepository.java
@@ -4,3 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
+public interface AllocationRuleRepository extends JpaRepository<AllocationRule, Long> {
+    boolean existsByRuleName(String ruleName);
+}
--- src/main/java/com/example/demo/repository/ResourceRequestRepository.java
+++ src/main/java/com/example/demo/repository/ResourceRequestRepository.java
@@ -4,7 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
-public interface ResourceRequestRepository extends JpaRepository<ResourceRequest, Long> {
+public interface ResourceAllocationRepository extends JpaRepository<ResourceAllocation, Long> { 
+ 
     List<ResourceRequest> findByRequestedBy_Id(Long id);
     List<ResourceRequest> findByStartTimeBetween(LocalDateTime a, LocalDateTime b);
 }
