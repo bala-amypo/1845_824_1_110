8d1e0d17cb5c3b2b9b7c03d7b3957f192d6f50bb
bala-amypo <bala@amypo.in>
2025-12-26 14:04:24 +0000

    User commit

--- src/main/java/com/example/demo/config/OpenApiConfig.java
+++ src/main/java/com/example/demo/config/OpenApiConfig.java
@@ -1,33 +1,19 @@
 package com.example.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
-
-        SecurityScheme bearerScheme =
-                new SecurityScheme()
-                        .type(SecurityScheme.Type.HTTP)
-                        .scheme("bearer")
-                        .bearerFormat("JWT");
-
         return new OpenAPI()
-                .info(new Info()
-                        .title("Resource Allocation Optimizer API")
-                        .description("Spring Boot REST API with JWT Security")
-                        .version("1.0"))
-                .addSecurityItem(
-                        new SecurityRequirement().addList("BearerAuth"))
-                .components(new Components()
-                        .addSecuritySchemes("BearerAuth", bearerScheme));
-    }
+                .servers(List.of(
+                        new Server().url("https://9049.32procr.amypo.ai/")
+                ));
+        }
 }
