1fac407e8d326b6b5ff5120fc2bf1ad36b336f3f
bala-amypo <bala@amypo.in>
2025-12-26 05:08:43 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/ResourceAllocationServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/ResourceAllocationServiceImpl.java
@@ -1,4 +1,4 @@
-package com.example.demo.service.Implementation;
+package com.example.demo.service.impl;
 
 import com.example.demo.entity.ResourceAllocation;
 import com.example.demo.entity.ResourceRequest;
