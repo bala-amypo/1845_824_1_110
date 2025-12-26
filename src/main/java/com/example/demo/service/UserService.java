372df89336bcf31e6538f72eefdbe6afd5eb6771
bala-amypo <bala@amypo.in>
2025-12-26 14:16:44 +0000

    User commit

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -1,10 +1,15 @@
 package com.example.demo.service;
 
 import com.example.demo.entity.User;
+import java.util.List;
 
 public interface UserService {
 
-    User register(User user);
+    User registerUser(User user);
+
+    User getUser(Long id);
+
+    List<User> getAllUsers();
 
     User findByEmail(String email);
 }
--- src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -3,6 +3,7 @@ package com.example.demo.service.impl;
 import com.example.demo.entity.User;
 import com.example.demo.repository.UserRepository;
 import com.example.demo.service.UserService;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -18,8 +19,19 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    public User register(User user) {
+    public User registerUser(User user) {
         return repository.save(user);
+    }
+
+    @Override
+    public User getUser(Long id) {
+        Optional<User> user = repository.findById(id);
+        return user.orElse(null);
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return repository.findAll();
     }
 
     @Override
