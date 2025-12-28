8870463227b956082d9ac8b4252aa67395f9450a
bala-amypo <bala@amypo.in>
2025-12-28 19:15:17 +0000

    User commit

--- src/main/java/com/example/demo/controller/ResourceAllocationController.java
+++ src/main/java/com/example/demo/controller/ResourceAllocationController.java
@@ -1,17 +1,3 @@
-// package com.example.demo.controller;
-
-// import com.example.demo.service.ResourceAllocationService;
-// import org.springframework.web.bind.annotation.RestController;
-
-// @RestController
-// public class ResourceAllocationController {
-
-//     private final ResourceAllocationService service;
-
-//     public ResourceAllocationController(ResourceAllocationService service) {
-//         this.service = service;
-//     }
-// }
 package com.example.demo.controller;
 
 import com.example.demo.entity.ResourceAllocation;
