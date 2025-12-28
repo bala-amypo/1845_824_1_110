8748e3cc7d24b34e5ee7c20fce14d8192c03d05a
bala-amypo <bala@amypo.in>
2025-12-28 19:12:17 +0000

    User commit

--- src/main/java/com/example/demo/controller/AllocationRuleController.java
+++ src/main/java/com/example/demo/controller/AllocationRuleController.java
@@ -1,32 +1,3 @@
-// package com.example.demo.controller;
-
-// import com.example.demo.entity.AllocationRule;
-// import com.example.demo.service.AllocationRuleService;
-// import java.util.List;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.web.bind.annotation.*;
-
-// @RestController
-// @RequestMapping("/api/allocation-rules")
-// public class AllocationRuleController {
-
-//     private final AllocationRuleService service;
-
-//     @Autowired
-//     public AllocationRuleController(AllocationRuleService service) {
-//         this.service = service;
-//     }
-
-//     @PostMapping
-//     public AllocationRule create(@RequestBody AllocationRule rule) {
-//         return service.createRule(rule);
-//     }
-
-//     @GetMapping
-//     public List<AllocationRule> getAll() {
-//         return service.getAllRules();
-//     }
-// }
 package com.example.demo.controller;
 
 import com.example.demo.entity.AllocationRule;
